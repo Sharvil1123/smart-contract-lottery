9b11255e88740f7c148e6abdca06bf906dd63003
Sharvil1123 <sharvilbhalke@gmail.com>
2024-05-01 20:04:32 +0530

    vrfv2Coordinator

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -23,6 +23,8 @@
 
 pragma solidity ^0.8.18;
 
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
 /**
  * @title A sample Raffle Contract
  * @author Sharvil Bhalke
@@ -30,26 +32,46 @@ pragma solidity ^0.8.18;
  * @dev Implements a ChainLink VRFv2
  */
 
-contract Raffle{
-    error Raffle__notEnoughEthSent() ;
+contract Raffle {
+    error Raffle__notEnoughEthSent();
+
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
     uint256 private immutable i_entranceFee; // immutable so that we can save gas.
     uint256 private immutable i_interval; // immutable, and set duration of lottery in seconds
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+
     address payable[] private s_players; // a array that stores addresses of players. stored in storage.
     uint256 private s_lastTimeStamp;
-    
+
     event EnteredRaffle(address indexed player);
 
-    constructor(uint256 entranceFee, uint256 interval){
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordindator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) {
         i_entranceFee = entranceFee;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordindator);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
-    function enterRaffle() public payable{
+    function enterRaffle() public payable {
         //the function is payable cause we want collect some eth in terms of fees.
         // use custom errors as they are more gas efficient than require statements.
         // require(msg.value >= i_entranceFee, "Not enough eth");
-        if(msg.value < i_entranceFee){
+        if (msg.value < i_entranceFee) {
             revert Raffle__notEnoughEthSent();
         }
         s_players.push(payable(msg.sender)); // when ebntered the lottery, the players(payable) add will be pushed to array
@@ -57,20 +79,30 @@ contract Raffle{
         emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() external{
+    function pickWinner() external {
         // 1. Get a random number
-        // 2. use the random number to pick a player    
+        // 2. use the random number to pick a player
         // 2. be automatically called.
 
         // check enough time is passed
-        if((block.timestamp - s_lastTimeStamp) < i_interval){
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
             revert();
         }
+
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        // request the rng
+        // get the random number.
     }
 
     /** Getter function */
 
-   function getEntranceFee() external view returns(uint256){
-    return i_entranceFee;
-   }
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
 }
